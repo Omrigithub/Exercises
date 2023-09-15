7f89c83c2f6d9999638ae0f98bb89f4921ed48a6
Omri Natanian <omrinatanian@gmail.com>
2023-09-15 15:41:47 +0300

    added solved string exercises

---
+++ main.py
@@ -0,0 +1,50 @@
+
+
+
+
+name = "Binyamin Netanyahu"
+
+# print first char
+print(name[0])
+
+# print 5th char
+print(name[5])
+
+# print last char
+print(name[-1])
+
+# print the 4th char from the end
+print(name[-4])
+
+# print first 3 char
+# name[start : end : jumps]
+print(name[0:3])
+
+# print first 6 char
+print(name[0:6])
+
+# print the second char till the 8th char
+print(name[1:9])
+
+# print every odd char
+print(name[1::2])
+
+# print the string from the 3rd to the 14th and every 3rd char
+print(name[2:14:3])
+
+# print every even char
+print(name[::2])
+# print the last 4 chars
+print(name[-4::])
+
+# print the last 8 chars
+print(name[-8::])
+
+# print the second word
+print(name.split(' ')[1])
+
+# print the string in reverse
+print(name[::-1])
+
+# print the first word
+print(name.split(' ')[0])
